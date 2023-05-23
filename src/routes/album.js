45fa7d026171707f926565ee7869b4041e0c702e
Max Tauroza <maxtauroza@gmail.com>
2023-05-23 13:29:37 +0100

    added test and functionality for delete request

--- src/routes/album.js
+++ src/routes/album.js
@@ -4,6 +4,7 @@ const {
   getAlbumById,
   updateAlbum,
   modifyAlbum,
+  deleteAlbum,
 } = require("../controllers/album");
 
 const albumRouter = express.Router();
@@ -12,6 +13,7 @@ albumRouter
   .get("/", getAllAlbums)
   .get("/:id", getAlbumById)
   .put("/:id", updateAlbum)
-  .patch("/:id", modifyAlbum);
+  .patch("/:id", modifyAlbum)
+  .delete("/:id", deleteAlbum);
 
 module.exports = albumRouter;
